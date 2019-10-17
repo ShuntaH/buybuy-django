f8f7432a420278ffa0f1879fcd5e33fe5e26e900
SHUNTA HOSAKA <hosaka@torico-tokyo.com>
2019-10-17 19:14:14 +0900

    update: 見た目70%ぐらい完成

--- project/memoapp/forms.py
+++ project/memoapp/forms.py
@@ -1,9 +1,10 @@
-from django import forms
+from django.forms import ModelForm, Textarea
 
 from .models import Day
 
 
-class DayCreateForm(forms.ModelForm):
+class DayCreateForm(ModelForm):
     class Meta:
         model = Day
+        widgets = {'parameters': Textarea(attrs={'cols': 35, 'rows': 6})}
         fields = '__all__'  # 特定のフィールドを指定したい場合はタップルで表記
