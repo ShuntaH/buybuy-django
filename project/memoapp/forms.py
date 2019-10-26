56ae5e3a7674b9bed30074313577d0f70aa36964
shunta <exec.apea@gmail.com>
2019-10-26 23:32:06 +0900

    Update: show paginations by include

--- project/memoapp/forms.py
+++ project/memoapp/forms.py
@@ -9,6 +9,7 @@ class DayCreateForm(ModelForm):
     class Meta:
         model = Day
         fields = '__all__'  # 特定のフィールドを指定したい場合はタップルで表記
+        # fields = ('title', 'text', 'date')
 
     def __init__(self, *args, **kwargs):
         super(DayCreateForm, self).__init__(*args, **kwargs)
@@ -16,7 +17,5 @@ class DayCreateForm(ModelForm):
         self.helper.form_method = 'post'
         self.helper.add_input(Submit('submit', 'Added'))
         self.fields['text'].widget.attrs.update({
-            'placeholder': 'You can take some notes'
+            'placeholder': 'This area is not required. And you can take some notes'
         })
-
-
--- project/memoapp/models.py
+++ project/memoapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -9,3 +10,7 @@ class Day(models.Model):
     title = models.CharField("What's on your mind?", max_length=200)
     text = models.TextField('Notes', blank=True)
     date = models.DateTimeField('When you hit it on...', default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
